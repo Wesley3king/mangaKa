23a09eb1763300abc8a5b7266ca75e464e08a5f1
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-05-02 22:04:04 -0300

    comaçando o projeto

---
+++ front-end/src/componentes/Menu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Menu extends React.Component {
+    render () {
+        return (
+            <>
+            <header className="header">
+                <div className="bt_menu"></div>
+                <div className="menu_icone"></div>
+                <div className="bt_search"></div>
+            </header>
+            </>
+        )
+    }
+}
