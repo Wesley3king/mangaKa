3bd9574e4490bcf067eef0a758b0f249c864708b
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-05-03 17:04:17 -0300

    ok

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Menu from "./componentes/Menu";
+import Barra from "./componentes/Barra";
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <Barra />
     </div>
   );
 }
--- front-end/src/componentes/Barra.jsx
+++ front-end/src/componentes/Barra.jsx
@@ -5,9 +5,41 @@ export default class Barra extends React.Component {
 
     render () {
         return (
-            <>
-            
-            </>
+            <div className="barra_menu">
+            <div className="close"></div>
+            <h1>mangaKa</h1>
+
+            <ul>
+                <li>
+                    <div className="fav"></div>
+                    <p>Favoritos</p>
+                </li>
+                <li>
+                    <div className="cat"></div>
+                    <p>Categorias</p>
+                </li>
+                <li>
+                    <div className="down"></div>
+                    <p>Downloads</p>
+                </li>
+                <li>
+                    <div className="coming"></div>
+                    <p>Em Breve</p>
+                </li>
+                <li>
+                    <div className="conf"></div>
+                    <p>Configurações</p>
+                </li>
+                <li>
+                    <div className="hom"></div>
+                    <p>Home</p>
+                </li>
+                <li>
+                    <div className="out"></div>
+                    <p>Sair</p>
+                </li>
+            </ul>
+            </div>
         )
     }
 }
