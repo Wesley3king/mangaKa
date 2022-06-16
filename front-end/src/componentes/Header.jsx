762ab3dc0b3351255de4bb82ae88094329cdf0c9
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-06-16 09:36:44 -0300

    primeiro uso do react-icons

--- front-end/src/componentes/Header.jsx
+++ front-end/src/componentes/Header.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { VscMenu } from "react-icons/vsc";
 
 export default class Header extends React.Component {
     render () {
         return (
             <>
             <header className="header">
-                <div className="bt_menu" onClick={()=> {
-    let menu = window.document.querySelector('.barra_menu');
-    menu.classList.toggle("ativa"); console.log("ddddd");
-  }}></div>
+            <VscMenu onClick={()=> {
+    let menu = window.document.querySelector('.barra_menu');menu.classList.toggle("ativa");}} className="bt_menu"/>
                 <div className="menu_icone">mangaKa</div>
                 <Link to="/pesquisar"><div className="bt_search"></div></Link>
             </header>
