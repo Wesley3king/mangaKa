c7e84937f42f8ffa270c8e0de71966e789806b8b
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-07-12 10:55:06 -0300

    new manga added

--- back-end/server.js
+++ back-end/server.js
@@ -14,10 +14,10 @@ app.use(routes);
 
 //adicionar (nome, url , length)
 // ultimo_cap_no_mangayabu = 2379 / ultimo_ready = 2313 / total_length = 67/78 / added = 457;   1933
-//func.adicionar_manga_especifico("One Punch-Man","https://mangayabu.top/manga/one-punch-man", null, null);
+func.adicionar_manga_especifico("Yuusha ga Shinda!: Murabito no Ore ga Hotta Otoshiana ni Yuusha ga Ochita Kekka","https://mangayabu.top/manga/yuusha-ga-shinda-murabito-no-ore-ga-hotta-otoshiana-ni-yuusha-ga-ochita-kekka", null, null);
 
 // fazer a atualização dos mangas no main
-func.vasculhar_main();
+//func.vasculhar_main();
 
 //inserir um capitulo preciso (nome, ['Capítulo #18', '917161'], position);
 //func.inserir_especifico();
